a7a2059305e235a0af13598af4e4a59e329ef33e
海法修平 <fnmmbestsk15kkpo@gmail.com>
2019-07-24 17:07:36 +0900

    [Update] Return bbox value

--- video_demo.py
+++ video_demo.py
@@ -41,15 +41,21 @@ def prep_image(img, inp_dim):
     img_ = torch.from_numpy(img_).float().div(255.0).unsqueeze(0)
     return img_, orig_im, dim
 
-def write(x, img):
+def write(x, img, classes, colors):
+    #classes = load_classes('data/coco.names')
+    #colors = pkl.load(open("pallete", "rb"))
     c1 = tuple(x[1:3].int())
     c2 = tuple(x[3:5].int())
     cls = int(x[-1])
     label = "{0}".format(classes[cls])
     color = random.choice(colors)
+    print("c1: ", c1)
+    print("c2 :", c2)
     cv2.rectangle(img, c1, c2,color, 1)
     t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1 , 1)[0]
     c2 = c1[0] + t_size[0] + 3, c1[1] + t_size[1] + 4
+    print("c1: ", c1)
+    print("c2 :", c2)
     cv2.rectangle(img, c1, c2,color, -1)
     cv2.putText(img, label, (c1[0], c1[1] + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [225,255,255], 1);
     return img
@@ -81,7 +87,8 @@ def arg_parse():
     return parser.parse_args()
 
 
-if __name__ == '__main__':
+#if __name__ == '__main__':
+def demo():
     args = arg_parse()
     confidence = float(args.confidence)
     nms_thesh = float(args.nms_thresh)
@@ -91,9 +98,9 @@ if __name__ == '__main__':
 
     num_classes = 80
 
-    CUDA = torch.cuda.is_available()
-    
     bbox_attrs = 5 + num_classes
+
+    bboxes = []
     
     print("Loading network.....")
     model = Darknet(args.cfgfile)
@@ -107,81 +114,94 @@ if __name__ == '__main__':
 
     if CUDA:
         model.cuda()
-        
+
     model(get_test_input(inp_dim, CUDA), CUDA)
 
     model.eval()
-    
+
     videofile = args.video
-    
+
     # set 0 for debug
     cap = cv2.VideoCapture(0)
-    
+
     assert cap.isOpened(), 'Cannot capture source'
-    
+
     frames = 0
-    start = time.time()    
+    start = time.time()
     while cap.isOpened():
-        
+
         ret, frame = cap.read()
+        print("ret: ", ret)
+        print("frame: ", frame.shape)
         if ret:
-            
+
 
             img, orig_im, dim = prep_image(frame, inp_dim)
-            
-            im_dim = torch.FloatTensor(dim).repeat(1,2)                        
-            
-            
+            im_dim = torch.FloatTensor(dim).repeat(1,2)
+
             if CUDA:
                 im_dim = im_dim.cuda()
                 img = img.cuda()
-            
-            with torch.no_grad():   
+
+            with torch.no_grad():
                 output = model(Variable(img), CUDA)
             output = write_results(output, confidence, num_classes, nms = True, nms_conf = nms_thesh)
 
             if type(output) == int:
                 frames += 1
+                print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
                 print("FPS of the video is {:5.2f}".format( frames / (time.time() - start)))
                 cv2.imshow("frame", orig_im)
                 key = cv2.waitKey(1)
                 if key & 0xFF == ord('q'):
                     break
                 continue
-            
-            
 
-            
             im_dim = im_dim.repeat(output.size(0), 1)
             scaling_factor = torch.min(inp_dim/im_dim,1)[0].view(-1,1)
-            
+
             output[:,[1,3]] -= (inp_dim - scaling_factor*im_dim[:,0].view(-1,1))/2
             output[:,[2,4]] -= (inp_dim - scaling_factor*im_dim[:,1].view(-1,1))/2
-            
+
             output[:,1:5] /= scaling_factor
-    
+
             for i in range(output.shape[0]):
                 output[i, [1,3]] = torch.clamp(output[i, [1,3]], 0.0, im_dim[i,0])
                 output[i, [2,4]] = torch.clamp(output[i, [2,4]], 0.0, im_dim[i,1])
-            
+
+            print("output: ", output)
+            print("output: ", output.shape)
+#            output = output.int().numpy()
+#            print("output: ", output)
+
+            for i in output:
+                x0 = i[1].int()
+                y0 = i[2].int()
+                x1 = i[3].int()
+                y1 = i[4].int()
+                bbox = (x0, y0, x1, y1)
+                bboxes.append(bbox)
+                print(bbox)
+
+
+            return bboxes
+
             classes = load_classes('data/coco.names')
             colors = pkl.load(open("pallete", "rb"))
-            
-            list(map(lambda x: write(x, orig_im), output))
-            
-            
+
+            # write bbox
+            list(map(lambda x: write(x, orig_im, classes, colors), output))
+
             cv2.imshow("frame", orig_im)
             key = cv2.waitKey(1)
             if key & 0xFF == ord('q'):
                 break
             frames += 1
-            print("FPS of the video is {:5.2f}".format( frames / (time.time() - start)))
+            print("FPS of the video is {:5.2f}g7".format( frames / (time.time() - start)))
 
-            
         else:
             break
-    
 
-    
-    
 
+if __name__ == '__main__':
+  demo()
